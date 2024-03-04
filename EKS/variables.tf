3b9567e79e57dd147be10aa2381549c2890fb5bf
Busayo Abati <abatiaisha@yahoo.com>
2024-03-04 14:00:43 -0500

    SAVE ME

---
+++ EKS/variables.tf
@@ -0,0 +1,18 @@
+
+variable "vpc_cidr" {
+  description = "vpc cidr"
+  type        = string
+
+
+}
+variable "public_subnets" {
+  description = "public subnets"
+  type        = list(string)
+
+}
+
+variable "private_subnets" {
+  description = "private subnets"
+  type        = list(string)
+
+}
