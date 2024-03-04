ea32cb2fe33dee6c2080d94792b56035ba09efa6
Busayo Abati <abatiaisha@yahoo.com>
2024-03-04 14:28:15 -0500

    save

--- EKS/main.tf
+++ EKS/main.tf
@@ -1,26 +1,38 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name                 = "jEKS-vpc"
+  name                 = "EKS-vpc"
   cidr                 = var.vpc_cidr
-  public_subnets       = var.public_subnets
+  private_subnets      = var.private_subnets
+  public_subnet_names   =var.public_subnets
   enable_dns_hostnames = true
   azs                  = data.aws_availability_zones.AZS.names
 
-  tags = {
-    Name        = "Jenkins-vpc"
-    Terraform   = "true"
-    Environment = "dev"
+ tags = {
+   
+                                          
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+ }
+
+ public_subnet_tags = {
+
+    "kubernetes.io/cluster/" = "shared"
+    "kubernetes.io/role/internal-elb"           = "1"
   }
-}
+ private_subnet_tags =  {
+
+    "kubernetes.io/cluster/" = "shared"
+    "kubernetes.io/role/internal-elb"           = "1"
+  }
+ }
+ 
 
 
-
-module "Jenkins_sg" {
+module "EKS_sg" {
   source = "terraform-aws-modules/security-group/aws"
 
-  name        = "Jenkins sg"
-  description = "Security group for Jenkins"
+  name        = "EKS sg"
+  description = "Security group for EKS"
   vpc_id      = module.vpc.vpc_id
 
 
@@ -52,28 +64,77 @@ module "Jenkins_sg" {
 
 }
 
-module "ec2_instance" {
-  source = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "Jenkins-server"
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
 
-  instance_type               = "t2.micro"
-  key_name                    = "my key_pair"
-  monitoring                  = true
-  vpc_security_group_ids      = [module.Jenkins_sg.security_group_id]
-  subnet_id                   = module.vpc.public_subnets[0]
-  associate_public_ip_address = true
-  user_data                   = file("install me.sh")
-  availability_zone           = data.aws_availability_zones.AZS.names[0]
+  cluster_name    = "EKS-cluster"
+  cluster_version = "1.29"
 
+  cluster_endpoint_public_access  = true
 
-  tags = {
-    Name        = "Jenkins-server"
-    Terraform   = "true"
-    Environment = "dev"
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
   }
 
+  vpc_id                   = "vpc-1234556abcdef"
+  subnet_ids               = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
+  control_plane_subnet_ids = ["subnet-xyzde987", "subnet-slkjf456", "subnet-qeiru789"]
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
+  }
+
+  eks_managed_node_groups = {
+    example = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 1
+
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+  # Cluster access entry
+  # To add the current caller identity as an administrator
+  enable_cluster_creator_admin_permissions = true
+
+  access_entries = {
+    # One access entry with a policy associated
+    example = {
+      kubernetes_groups = []
+      principal_arn     = "arn:aws:iam::123456789012:role/something"
+
+      policy_associations = {
+        example = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
+          access_scope = {
+            namespaces = ["default"]
+            type       = "namespace"
+          }
+        }
+      }
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
+  
+
 output "public_ip" {
   value = module.ec2_instance.public_ip
 }
--- Jenkins server/main.tf
+++ Jenkins server/main.tf
@@ -52,80 +52,30 @@ module "Jenkins_sg" {
 
 }
 
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
 
-  cluster_name    = "EKS-cluster"
-  cluster_version = "1.29"
+module "ec2_instance" {
+  source = "terraform-aws-modules/ec2-instance/aws"
 
-  cluster_endpoint_public_access  = true
+  name = "Jenkins-server"
 
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-  }
+  instance_type               = "t2.micro"
+  key_name                    = "my key_pair"
+  monitoring                  = true
+  vpc_security_group_ids      = [module.Jenkins_sg.security_group_id]
+  subnet_id                   = module.vpc.public_subnets[0]
+  associate_public_ip_address = true
+  user_data                   = file("install me.sh")
+  availability_zone           = data.aws_availability_zones.AZS.names[0]
 
-  vpc_id                   = "vpc-1234556abcdef"
-  subnet_ids               = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
-  control_plane_subnet_ids = ["subnet-xyzde987", "subnet-slkjf456", "subnet-qeiru789"]
-
-  # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
-  }
-
-  eks_managed_node_groups = {
-    example = {
-      min_size     = 1
-      max_size     = 10
-      desired_size = 1
-
-      instance_types = ["t3.large"]
-      capacity_type  = "SPOT"
-    }
-  }
-
-  # Cluster access entry
-  # To add the current caller identity as an administrator
-  enable_cluster_creator_admin_permissions = true
-
-  access_entries = {
-    # One access entry with a policy associated
-    example = {
-      kubernetes_groups = []
-      principal_arn     = "arn:aws:iam::123456789012:role/something"
-
-      policy_associations = {
-        example = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
-          access_scope = {
-            namespaces = ["default"]
-            type       = "namespace"
-          }
-        }
-      }
-    }
-  }
 
   tags = {
-    Environment = "dev"
+    Name        = "Jenkins-server"
     Terraform   = "true"
-  }
-}
+    Environment = "dev"
   }
 
 }
 output "public_ip" {
   value = module.ec2_instance.public_ip
 }
-
-
 
