d09ac4f17690f669e62e08c1b0b726d35a4f9691
Busayor123 <131611646+Busayor123@users.noreply.github.com>
2024-06-16 23:14:07 -0400

    Update install tools.sh

--- Jenkins server/install tools.sh
+++ Jenkins server/install tools.sh
@@ -8,12 +8,12 @@
 
  sudo yum update –y
  sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+ https://pkg.jenkins.io/redhat-stable/jenkins.repo
  sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
  sudo yum upgrade -y
-sudo amazon-linux-extras install java-openjdk11 -y
-sudo yum install jenkins -y
-sudo systemctl enable jenkins
+ sudo amazon-linux-extras install java-openjdk11 -y
+ sudo yum install jenkins -y
+ sudo systemctl enable jenkins
  sudo systemctl start jenkins
  sudo systemctl status jenkins
 
